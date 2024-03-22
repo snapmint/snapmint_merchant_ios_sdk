29a4562318240c4bfebe3aedeb0867e552e69198
lokeshsnapmint <146188815+lokeshsnapmint@users.noreply.github.com>
2024-03-22 19:00:17 +0530

    Sdk Name Change

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "IosMerchantSdk",
+    name: "SnapmintMerchantSdk",
     platforms: [
       .macOS(.v12), .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "IosMerchantSdk",
-            targets: ["IosMerchantSdk"]),
+            name: "SnapmintMerchantSdk",
+            targets: ["SnapmintMerchantSdk"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "IosMerchantSdk",
-            path: "./Sources/IosMerchantSdk.xcframework")
+            name: "SnapmintMerchantSdk",
+            path: "./Sources/SnapmintMerchantSdk.xcframework")
     ]
 )
