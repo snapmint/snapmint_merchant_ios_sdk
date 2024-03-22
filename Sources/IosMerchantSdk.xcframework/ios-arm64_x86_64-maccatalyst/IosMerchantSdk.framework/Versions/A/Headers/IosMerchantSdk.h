e596f7e5e60b9cbce90da55c21e84d9bca9f474d
lokeshsnapmint <146188815+lokeshsnapmint@users.noreply.github.com>
2024-03-22 15:46:56 +0530

    add snapmint sdk

---
+++ Sources/IosMerchantSdk.xcframework/ios-arm64_x86_64-maccatalyst/IosMerchantSdk.framework/Versions/A/Headers/IosMerchantSdk.h
@@ -0,0 +1,18 @@
+//
+//  IosMerchantSdk.h
+//  IosMerchantSdk
+//
+//  Created by Lokesh Joshi on 20/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IosMerchantSdk.
+FOUNDATION_EXPORT double IosMerchantSdkVersionNumber;
+
+//! Project version string for IosMerchantSdk.
+FOUNDATION_EXPORT const unsigned char IosMerchantSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IosMerchantSdk/PublicHeader.h>
+
+
