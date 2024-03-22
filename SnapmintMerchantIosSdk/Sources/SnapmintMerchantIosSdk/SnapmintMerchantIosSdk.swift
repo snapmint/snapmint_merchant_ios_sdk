e60b6f8f39b4990441a990352710a15623e1109a
lokeshsnapmint <146188815+lokeshsnapmint@users.noreply.github.com>
2024-03-22 13:06:31 +0530

    init commit

---
+++ SnapmintMerchantIosSdk/Sources/SnapmintMerchantIosSdk/SnapmintMerchantIosSdk.swift
@@ -0,0 +1,19 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct SnapmintMerchantIosSdk: View {
+    
+    public init() {
+        // Initialization code
+    }
+    
+    public var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! Lokesh")
+        }
+        .padding()
+    }
+}
---
+++ SnapmintMerchantIosSdk/Tests/SnapmintMerchantIosSdkTests/SnapmintMerchantIosSdkTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SnapmintMerchantIosSdk
+
+final class SnapmintMerchantIosSdkTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
